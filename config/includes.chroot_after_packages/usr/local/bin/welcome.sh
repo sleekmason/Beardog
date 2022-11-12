a3f13e76fbcbd9edee7cd45f9689d4ac7a89cc12
sleekmason <sleekmason@gmail.com>
2022-11-12 16:08:27 -0600

    Full theme change to Dark-Earth.

--- config/includes.chroot_after_packages/usr/local/bin/welcome.sh
+++ config/includes.chroot_after_packages/usr/local/bin/welcome.sh
@@ -7,8 +7,8 @@ if grep -q "$fs" /proc/partitions; then
    sed -i '/welcome.sh &/d' ~/.config/openbox/autostart && exit
 else
   yad --title "Welcome!" --window-icon=/usr/share/icons/ld-icons/paw-color.png \
---width=385 --height=340 --center --escape-ok --undecorated \
+--width=375 --height=340 --center --escape-ok --undecorated \
 --skip-taskbar  --button="gtk-ok:0" \
 --text-info --justify=left --wrap < /usr/share/lilidog/welcome.txt --fontname="Dejavu Sans 11" \
---fore="#ABC7F5"; sed -i '/welcome.sh &/d' ~/.config/openbox/autostart
+--fore="#DBDBDB"; sed -i '/welcome.sh &/d' ~/.config/openbox/autostart
 fi
