e6cb63fdf45a91e35d402bfb725a6bc797158c13
sleekmason <sleekmason@gmail.com>
2022-11-20 11:58:14 -0600

    initial setup for welcome script

--- config/includes.chroot_after_packages/usr/local/bin/welcome.sh
+++ config/includes.chroot_after_packages/usr/local/bin/welcome.sh
@@ -4,11 +4,16 @@
 full_fs=$(df ~ | tail -1 | awk '{print $1;}')  # find partition
 fs=$(basename "$full_fs")
 if grep -q "$fs" /proc/partitions; then
-   sed -i '/welcome.sh &/d' ~/.config/openbox/autostart && exit
+  yad --title "Welcome!" --window-icon=/usr/share/icons/ld-icons/paw-color.png \
+--width=375 --height=340 --center --escape-ok --undecorated --skip-taskbar \
+--button=" Begin"!/usr/share/icons/Adwaita/22x22/legacy/preferences-desktop-font.png!:"x-terminal-emulator -T 'Customization' -e sudo xentry -i; bash" \
+--button="gtk-ok:0" \
+--text-info --justify=left --wrap < /usr/share/lilidog/welcome.txt --fontname="Dejavu Sans 11" \
+--fore="#DBDBDB"; sed -i '/welcome.sh &/d' ~/.config/openbox/autostart && exit
 else
   yad --title "Welcome!" --window-icon=/usr/share/icons/ld-icons/paw-color.png \
---width=375 --height=340 --center --escape-ok --undecorated \
---skip-taskbar  --button="gtk-ok:0" \
---text-info --justify=left --wrap < /usr/share/lilidog/welcome.txt --fontname="Dejavu Sans 11" \
+--width=375 --height=340 --center --escape-ok --undecorated --skip-taskbar \
+--button="gtk-ok:0" \
+--text-info --justify=left --wrap < /usr/share/lilidog/welcome2.txt --fontname="Dejavu Sans 11" \
 --fore="#DBDBDB"; sed -i '/welcome.sh &/d' ~/.config/openbox/autostart
 fi
