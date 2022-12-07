783d72ca465d85c4bef5971c59c826fd169ba9f7
sleekmason <sleekmason@gmail.com>
2022-12-07 12:23:41 -0600

    xentry - added reboot and adjusted varios wording and geometry. xupdate - adjusted wording and geometry.

--- config/includes.chroot_after_packages/usr/local/bin/welcome.sh
+++ config/includes.chroot_after_packages/usr/local/bin/welcome.sh
@@ -5,15 +5,15 @@ full_fs=$(df ~ | tail -1 | awk '{print $1;}')  # find partition
 fs=$(basename "$full_fs")
 if grep -q "$fs" /proc/partitions; then
   yad --title "Welcome!" --window-icon=/usr/share/icons/ld-icons/paw-color.png \
---width=375 --height=340 --center --escape-ok --undecorated --skip-taskbar \
---button=" Begin"!/usr/share/icons/Adwaita/22x22/legacy/preferences-desktop-font.png!:"x-terminal-emulator -T 'Customization' -e sudo xentry -i" \
+--width=375 --height=440 --center --escape-ok --undecorated --skip-taskbar \
+--button=" Begin"!/usr/share/icons/Adwaita/22x22/legacy/preferences-desktop-font.png!:"lxterminal --geometry=68x20-80-80 -T 'Customization' -e 'sudo xentry -i'" \
 --button="gtk-ok:0" \
---text-info --justify=left --wrap < /usr/share/lilidog/welcome.txt --fontname="Dejavu Sans 11" \
+--text-info --justify=left --wrap < /usr/share/lilidog/welcome.txt --fontname="Sans 11" \
 --fore="#DBDBDB"; sed -i '/welcome.sh &/d' ~/.config/openbox/autostart && exit
 else
   yad --title "Welcome!" --window-icon=/usr/share/icons/ld-icons/paw-color.png \
 --width=375 --height=340 --center --escape-ok --undecorated --skip-taskbar \
 --button="gtk-ok:0" \
---text-info --justify=left --wrap < /usr/share/lilidog/welcome2.txt --fontname="Dejavu Sans 11" \
+--text-info --justify=left --wrap < /usr/share/lilidog/welcome2.txt --fontname="Sans 11" \
 --fore="#DBDBDB"; sed -i '/welcome.sh &/d' ~/.config/openbox/autostart
 fi
