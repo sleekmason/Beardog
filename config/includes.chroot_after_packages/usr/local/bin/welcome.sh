06d4df1c603eed3773bf8879af7321f938a7f0e4
sleekmason <sleekmason@gmail.com>
2023-02-21 15:46:17 -0600

    adjusted Welcome text color.

--- config/includes.chroot_after_packages/usr/local/bin/welcome.sh
+++ config/includes.chroot_after_packages/usr/local/bin/welcome.sh
@@ -9,11 +9,11 @@ if grep -q "$fs" /proc/partitions; then
 --button=" Begin"!/usr/share/icons/Adwaita/22x22/legacy/preferences-desktop-font.png!:"x-terminal-emulator -T 'Customization' -e 'sudo xentry -i'" \
 --button="gtk-ok:0" \
 --text-info --justify=left --wrap < /usr/share/lilidog/welcome.txt --fontname="Sans 11" \
---fore="#DBDBDB"; sed -i '/welcome.sh &/d' ~/.config/openbox/autostart && exit
+--fore="#C0DDEB"; sed -i '/welcome.sh &/d' ~/.config/openbox/autostart && exit
 else
   yad --title "Welcome!" --window-icon=/usr/share/icons/ld-icons/paw-color.png \
 --width=375 --height=330 --center --escape-ok --undecorated --skip-taskbar \
 --button="gtk-ok:0" \
 --text-info --justify=left --wrap < /usr/share/lilidog/welcome2.txt --fontname="Sans 11" \
---fore="#DBDBDB"; sed -i '/welcome.sh &/d' ~/.config/openbox/autostart
+--fore="#C0DDEB"; sed -i '/welcome.sh &/d' ~/.config/openbox/autostart
 fi
